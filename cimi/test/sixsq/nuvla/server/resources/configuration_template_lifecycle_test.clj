4e9d9b105192c1136080180916633660ddeb1b1c
Charles (Cal) Loomis <cal@sixsq.com>
2019-02-08 07:42:33 +0100

    remove references to deleted namespaces (#17)

--- cimi/test/sixsq/nuvla/server/resources/configuration_template_lifecycle_test.clj
+++ cimi/test/sixsq/nuvla/server/resources/configuration_template_lifecycle_test.clj
@@ -5,10 +5,6 @@
     [sixsq.nuvla.server.resources.common.utils :as u]
     [sixsq.nuvla.server.resources.configuration-template :as ct]
     [sixsq.nuvla.server.resources.configuration-template-lifecycle-test-utils :as test-utils]
-    [sixsq.nuvla.server.resources.configuration-template-session-github :as github]
-    [sixsq.nuvla.server.resources.configuration-template-session-mitreid :as mitreid]
-    [sixsq.nuvla.server.resources.configuration-template-session-mitreid-token :as mitreid-token]
-    [sixsq.nuvla.server.resources.configuration-template-session-oidc :as oidc]
     [sixsq.nuvla.server.resources.configuration-template-slipstream :as slipstream]
     [sixsq.nuvla.server.resources.lifecycle-test-utils :as ltu]))
 
@@ -19,18 +15,10 @@
 
 
 (deftest retrieve-by-id
-  (test-utils/check-retrieve-by-id slipstream/service)
-  (test-utils/check-retrieve-by-id oidc/service)
-  (test-utils/check-retrieve-by-id mitreid/service)
-  (test-utils/check-retrieve-by-id mitreid-token/service)
-  (test-utils/check-retrieve-by-id github/service))
+  (test-utils/check-retrieve-by-id slipstream/service))
 
 (deftest lifecycle
-  (test-utils/check-lifecycle slipstream/service)
-  (test-utils/check-lifecycle oidc/service)
-  (test-utils/check-lifecycle mitreid/service)
-  (test-utils/check-lifecycle mitreid-token/service)
-  (test-utils/check-lifecycle github/service))
+  (test-utils/check-lifecycle slipstream/service))
 
 (deftest bad-methods
   (test-utils/check-bad-methods))
