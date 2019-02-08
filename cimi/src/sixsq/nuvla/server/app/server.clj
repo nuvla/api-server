fd84c16d2101884ab9617cd1ca78ad73f67b262e
Charles Loomis <cal@sixsq.com>
2019-02-08 07:53:13 +0100

    remove metrics

--- cimi/src/sixsq/nuvla/server/app/server.clj
+++ cimi/src/sixsq/nuvla/server/app/server.clj
@@ -4,8 +4,6 @@
     [sixsq.nuvla.db.ephemeral-impl :as edb]
     [sixsq.nuvla.db.impl :as db]
     [sixsq.nuvla.db.loader :as db-loader]
-    [sixsq.nuvla.server.app.graphite :as graphite]
-    [sixsq.nuvla.server.app.params :as p]
     [sixsq.nuvla.server.app.routes :as routes]
     [sixsq.nuvla.server.middleware.authn-info-header :refer [wrap-authn-info-header]]
     [sixsq.nuvla.server.middleware.base-uri :refer [wrap-base-uri]]
@@ -15,10 +13,6 @@
     [sixsq.nuvla.server.resources.common.dynamic-load :as resources]
     [sixsq.nuvla.server.util.zookeeper :as zku]
     [environ.core :as env]
-    [metrics.core :refer [default-registry remove-all-metrics]]
-    [metrics.jvm.core :refer [instrument-jvm]]
-    [metrics.ring.expose :refer [expose-metrics-as-json]]
-    [metrics.ring.instrument :refer [instrument]]
     [ring.middleware.cookies :refer [wrap-cookies]]
     [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
     [ring.middleware.keyword-params :refer [wrap-keyword-params]]
@@ -48,10 +42,8 @@
       wrap-params
       wrap-exceptions
       wrap-authn-info-header
-      (expose-metrics-as-json (str p/service-context "metrics") default-registry {:pretty-print? true})
       (wrap-json-body {:keywords? true})
       (wrap-json-response {:pretty true :escape-non-ascii true})
-      (instrument default-registry)
       wrap-logger
       wrap-cookies))
 
@@ -60,7 +52,6 @@
   "Stops the application server by calling the function that was
    created when the application server was started."
   []
-  (graphite/stop-graphite-reporter)
 
   (try
     (zku/close-client!)
@@ -73,7 +64,6 @@
   (edb/close)
 
   (try
-    (remove-all-metrics)
     (log/info "removed all instrumentation metrics")
     (catch Exception e
       (log/warn "failed removing all instrumentation metrics:" (str e)))))
@@ -83,7 +73,6 @@
   []
 
   (try
-    (instrument-jvm default-registry)
     (catch Exception e
       (log/warn "error registering instrumentation metrics:" (str e))))
 
@@ -102,8 +91,6 @@
     (resources/initialize)
     (catch Exception e
       (log/warn "error initializing resources:" (str e))))
-
-  (graphite/start-graphite-reporter)
 
   ;; returns tuple with handler and stop function
   [(create-ring-handler) stop])
