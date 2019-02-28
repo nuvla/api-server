a4d94b366e91e8f0013509a49007f4c165e58b21
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-02-28 16:34:27 +0100

    add aws template

---
+++ cimi/src/sixsq/nuvla/server/resources/credential_service_aws.clj
@@ -0,0 +1,50 @@
+(ns sixsq.nuvla.server.resources.credential-service-aws
+    "
+Sets the service compliant attribute names and values
+for AWS
+"
+    (:require
+      [sixsq.nuvla.server.resources.common.std-crud :as std-crud]
+      [sixsq.nuvla.server.resources.common.utils :as u]
+      [sixsq.nuvla.server.resources.credential :as p]
+      [sixsq.nuvla.server.resources.credential-template-service-aws :as tpl]
+      [sixsq.nuvla.server.resources.spec.credential-service-aws :as service]))
+
+;;
+;; convert template to credential
+;;
+
+(defmethod p/tpl->credential tpl/credential-type
+           [{:keys [type method amazonec2-access-key amazonec2-secret-key services acl]} request]
+           (let [resource (cond-> {:resource-type        p/resource-type
+                                   :type                 type
+                                   :method               method
+                                   :amazonec2-access-key amazonec2-access-key
+                                   :amazonec2-secret-key amazonec2-secret-key
+                                   :services             services}
+                                  acl (assoc :acl acl))]
+                [nil resource]))
+
+
+;;
+;; multimethods for validation
+;;
+
+(def validate-fn (u/create-spec-validation-fn ::service/schema))
+(defmethod p/validate-subtype tpl/credential-type
+           [resource]
+           (validate-fn resource))
+
+(def create-validate-fn (u/create-spec-validation-fn ::service/schema-create))
+(defmethod p/create-validate-subtype tpl/credential-type
+           [resource]
+           (create-validate-fn resource))
+
+
+;;
+;; initialization
+;;
+
+(defn initialize
+      []
+      (std-crud/initialize p/resource-type ::service/schema))
--- cimi/src/sixsq/nuvla/server/resources/credential_template_service_aws.clj
+++ cimi/src/sixsq/nuvla/server/resources/credential_template_service_aws.clj
@@ -1,21 +1,21 @@
-(ns sixsq.nuvla.server.resources.credential-template-service-exoscale
+(ns sixsq.nuvla.server.resources.credential-template-service-aws
     "This CredentialTemplate allows creating a Credential instance to hold
-    cloud credentials for the Exoscale's services."
+    cloud credentials for the AWS's services."
     (:require
       [sixsq.nuvla.server.resources.credential-template :as p]
       [sixsq.nuvla.server.resources.common.utils :as u]
       [sixsq.nuvla.server.resources.resource-metadata :as md]
       [sixsq.nuvla.server.util.metadata :as gen-md]
-      [sixsq.nuvla.server.resources.spec.credential-template-service-exoscale :as service]))
+      [sixsq.nuvla.server.resources.spec.credential-template-service-aws :as service]))
 
 
-(def ^:const credential-type "cloud-service-cred-exoscale")
+(def ^:const credential-type "cloud-service-cred-aws")
 
 
-(def ^:const resource-name "Exoscale API keys")
+(def ^:const resource-name "AWS API keys")
 
 
-(def ^:const method "store-cloud-service-cred-exoscale")
+(def ^:const method "store-cloud-service-cred-aws")
 
 
 (def resource-acl {:owner {:principal "ADMIN"
@@ -29,15 +29,15 @@
 ;;
 
 (def ^:const resource
-  {:type                    credential-type
-   :method                  method
-   :name                    resource-name
-   :description             "Exoscale cloud credentials"
-   :exoscale-api-key        ""
-   :exoscale-api-secret-key ""
-   :services                []
-   :acl                     resource-acl
-   :resourceMetadata        "resource-metadata/credential-template-driver-exoscale"})
+  {:type                 credential-type
+   :method               method
+   :name                 resource-name
+   :description          "AWS credentials"
+   :amazonec2-access-key ""
+   :amazonec2-secret-key ""
+   :services             []
+   :acl                  resource-acl
+   :resourceMetadata     "resource-metadata/credential-template-driver-aws"})
 
 
 ;;
