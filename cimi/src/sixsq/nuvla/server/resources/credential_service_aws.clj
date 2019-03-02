ccbb2c12c5963dfc71aa189cc0ea425a84750c91
Charles Loomis <cal@sixsq.com>
2019-03-02 16:17:01 +0100

    fix formatting

--- cimi/src/sixsq/nuvla/server/resources/credential_service_aws.clj
+++ cimi/src/sixsq/nuvla/server/resources/credential_service_aws.clj
@@ -1,29 +1,29 @@
 (ns sixsq.nuvla.server.resources.credential-service-aws
-    "
+  "
 Sets the service compliant attribute names and values
 for AWS
 "
-    (:require
-      [sixsq.nuvla.server.resources.common.std-crud :as std-crud]
-      [sixsq.nuvla.server.resources.common.utils :as u]
-      [sixsq.nuvla.server.resources.credential :as p]
-      [sixsq.nuvla.server.resources.credential-template-service-aws :as tpl]
-      [sixsq.nuvla.server.resources.spec.credential-service-aws :as service]))
+  (:require
+    [sixsq.nuvla.server.resources.common.std-crud :as std-crud]
+    [sixsq.nuvla.server.resources.common.utils :as u]
+    [sixsq.nuvla.server.resources.credential :as p]
+    [sixsq.nuvla.server.resources.credential-template-service-aws :as tpl]
+    [sixsq.nuvla.server.resources.spec.credential-service-aws :as service]))
 
 ;;
 ;; convert template to credential
 ;;
 
 (defmethod p/tpl->credential tpl/credential-type
-           [{:keys [type method amazonec2-access-key amazonec2-secret-key services acl]} request]
-           (let [resource (cond-> {:resource-type        p/resource-type
-                                   :type                 type
-                                   :method               method
-                                   :amazonec2-access-key amazonec2-access-key
-                                   :amazonec2-secret-key amazonec2-secret-key
-                                   :services             services}
-                                  acl (assoc :acl acl))]
-                [nil resource]))
+  [{:keys [type method amazonec2-access-key amazonec2-secret-key services acl]} request]
+  (let [resource (cond-> {:resource-type        p/resource-type
+                          :type                 type
+                          :method               method
+                          :amazonec2-access-key amazonec2-access-key
+                          :amazonec2-secret-key amazonec2-secret-key
+                          :services             services}
+                         acl (assoc :acl acl))]
+    [nil resource]))
 
 
 ;;
@@ -32,13 +32,13 @@ for AWS
 
 (def validate-fn (u/create-spec-validation-fn ::service/schema))
 (defmethod p/validate-subtype tpl/credential-type
-           [resource]
-           (validate-fn resource))
+  [resource]
+  (validate-fn resource))
 
 (def create-validate-fn (u/create-spec-validation-fn ::service/schema-create))
 (defmethod p/create-validate-subtype tpl/credential-type
-           [resource]
-           (create-validate-fn resource))
+  [resource]
+  (create-validate-fn resource))
 
 
 ;;
@@ -46,5 +46,5 @@ for AWS
 ;;
 
 (defn initialize
-      []
-      (std-crud/initialize p/resource-type ::service/schema))
+  []
+  (std-crud/initialize p/resource-type ::service/schema))
