5d8f1c6b0c895f380f5d2e165644df398e387a94
khaled basbous <khaled@sixsq.com>
2019-03-13 12:28:14 +0100

    fix unit tests

--- code/test/sixsq/nuvla/server/resources/session_template_internal_lifecycle_test.clj
+++ code/test/sixsq/nuvla/server/resources/session_template_internal_lifecycle_test.clj
@@ -29,64 +29,10 @@
                      :acl         st/resource-acl})
 
 
-(defn check-existing-session-template [base-uri valid-template]
-
-  (let [method (:method valid-template)
-        session (-> (ltu/ring-app)
-                    session
-                    (content-type "application/json"))
-        session-admin (header session authn-info-header "root ADMIN USER ANON")
-        session-anon (header session authn-info-header "unknown ANON")]
-
-    ;; should be an existing template already
-    (-> session-admin
-        (request base-uri
-                 :request-method :post
-                 :body (json/write-str valid-template))
-        (ltu/body->edn)
-        (ltu/is-status 409))
-
-    ;; session template should be visible via query as well
-    ;; should have one with the correct method name
-    (let [entries (-> session-anon
-                      (request base-uri)
-                      (ltu/body->edn)
-                      (ltu/is-status 200)
-                      (ltu/is-resource-uri st/collection-type)
-                      (ltu/entries))]
-      (is (= 1 (count (filter #(= method (:method %)) entries)))))
-
-    ;; do full lifecycle for an internal session template
-    (let [uri (str st/resource-type "/" method)
-          abs-uri (str p/service-context uri)]
-
-      ;; delete the template
-      (-> session-admin
-          (request abs-uri :request-method :delete)
-          (ltu/body->edn)
-          (ltu/is-status 200))
-
-      ;; verify that the template is gone
-      (-> session-admin
-          (request abs-uri)
-          (ltu/body->edn)
-          (ltu/is-status 404))
-
-      ;; session template should not be there anymore
-      (ltu/refresh-es-indices)
-      (let [entries (-> session-anon
-                        (request base-uri)
-                        (ltu/body->edn)
-                        (ltu/is-status 200)
-                        (ltu/is-resource-uri st/collection-type)
-                        (ltu/entries))]
-        (is (zero? (count (filter #(= method (:method %)) entries))))))))
-
-
 (deftest check-metadata
   (mdtu/check-metadata-exists (str st/resource-type "-" internal/resource-url)))
 
 
 (deftest lifecycle
-  (check-existing-session-template base-uri valid-template)
+  (stu/check-existing-session-template base-uri valid-template)
   (stu/session-template-lifecycle base-uri valid-template))
