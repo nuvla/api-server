836cb99a4ce1b58aec8750fa6253b2aa6bf8edda
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-03-18 16:36:37 +0100

    import db

--- code/src/sixsq/nuvla/server/resources/infrastructure_service.clj
+++ code/src/sixsq/nuvla/server/resources/infrastructure_service.clj
@@ -12,6 +12,7 @@ existing infrastructure-service-template resource.
   (:require
     [sixsq.nuvla.auth.acl :as a]
     [sixsq.nuvla.server.resources.common.crud :as crud]
+    [sixsq.nuvla.db.impl :as db]
     [sixsq.nuvla.server.resources.common.std-crud :as std-crud]
     [sixsq.nuvla.server.resources.common.utils :as u]
     [sixsq.nuvla.server.resources.resource-metadata :as md]
