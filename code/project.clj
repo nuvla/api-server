471fc3fd4c525a14aeeeedce087657676d31b922
Charles Loomis <cal@sixsq.com>
2019-04-17 13:25:13 +0200

    add configuration for release

--- code/project.clj
+++ code/project.clj
@@ -1,7 +1,7 @@
 (def parent-version "6.4.0")
-(def nuvla-ring-version "0.0.1-SNAPSHOT")
+(def nuvla-ring-version "0.9.0")
 
-(defproject sixsq.nuvla.server/api-jar "0.0.1-SNAPSHOT"
+(defproject sixsq.nuvla.server/api-jar "0.9.0-SNAPSHOT"
 
   :description "core api server"
 
