3c62e0f230593399493bea1d1d7aadc5996c0c28
khaled basbous <khaled@sixsq.com>
2019-04-25 17:35:08 +0200

    wip add implicit rights

--- code/src/sixsq/nuvla/auth/utils/acl.clj
+++ code/src/sixsq/nuvla/auth/utils/acl.clj
@@ -1,6 +1,8 @@
 (ns sixsq.nuvla.auth.utils.acl
   (:require
-    [clojure.set :as set]))
+    [clojure.set :as set]
+    [sixsq.nuvla.auth.acl-resource :as acl-resource]
+    [clojure.tools.logging :as log]))
 
 
 (defn val-as-set
@@ -24,7 +26,7 @@
 
 
 (defn normalize-acl
-  "Takes an ACL and returns the a normalized version of the ACL where all
+  "Takes an ACL and returns a normalized version of the ACL where all
    rights are listed explicitly and owners do not appear in the lists for
    individual rights."
   [{:keys [owners] :as acl}]
@@ -35,6 +37,33 @@
        (remove (fn [[k v]] (empty? v)))
        (map val-as-vector)
        (into {})))
+
+(defn kb-remove-owners-from-principals
+  [owners-set [right principals]]
+  [right (set/difference principals owners-set)])
+
+(defn kb-explode-rights
+  [[right principals]]
+  (let [qualified-right (get acl-resource/rights-keywords right)
+        sub-rights (ancestors acl-resource/rights-hierarchy qualified-right)]
+    (conj
+      (map (fn [sub-right] {sub-right principals}) sub-rights)
+      {qualified-right principals})))
+
+(defn kb-normalize-acl
+  "Takes an ACL and returns a normalized version of the ACL where all
+   rights are listed explicitly and owners do not appear in the lists for
+   individual rights."
+  [{:keys [owners] :as acl}]
+  (let [owners-set (set owners)
+        acl-rights (->> (dissoc acl :owners)
+                        (map val-as-set))]
+    (->> acl-rights
+         (map (partial kb-remove-owners-from-principals owners-set))
+         (map kb-explode-rights)
+         (reduce concat)
+         (reduce (fn [a b] (merge-with set/union a b))))))
+
 
 
 (defn minimal-acl
