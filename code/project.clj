91988af45e285b705350085ab2e77af12f70b1e0
Charles Loomis <cal@sixsq.com>
2019-05-13 14:59:51 -0400

    next development version

--- code/project.clj
+++ code/project.clj
@@ -1,7 +1,7 @@
 (def parent-version "6.5.0")
 (def nuvla-ring-version "2.0.0")
 
-(defproject sixsq.nuvla.server/api-jar "2.1.1"
+(defproject sixsq.nuvla.server/api-jar "2.1.2-SNAPSHOT"
 
   :description "core api server"
 
