4a69c0721305b0bf92f4316f1efc4f1d1d7daa4c
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2019-08-13 09:11:25 +0200

    update module component callback

--- code/src/sixsq/nuvla/server/resources/callback_module_update.clj
+++ code/src/sixsq/nuvla/server/resources/callback_module_update.clj
@@ -1,11 +1,12 @@
 (ns sixsq.nuvla.server.resources.callback-module-update
   (:require
     [clojure.tools.logging :as log]
-    [sixsq.nuvla.db.impl :as db]
+    [sixsq.nuvla.auth.utils :as auth]
     [sixsq.nuvla.server.resources.notification :refer [resource-type]]
     [sixsq.nuvla.server.resources.callback :as callback]
     [sixsq.nuvla.server.resources.common.crud :as crud]
     [sixsq.nuvla.server.resources.common.utils :as u]
+    [sixsq.nuvla.server.resources.module :as m]
     [sixsq.nuvla.server.util.response :as r]))
 
 
@@ -14,12 +15,20 @@
 
 (defn update-component!
   [module-id data]
-  (try
-    (-> (crud/retrieve-by-id-as-admin module-id)
-        (merge data)
-        (db/edit nil))
-    (catch Exception e
-      (or (ex-data e) (throw e)))))
+  (let [module (crud/retrieve-by-id module-id)
+        new-content (-> module
+                        :versions
+                        last
+                        :href
+                        crud/retrieve-by-id
+                        (merge data)
+                        u/strip-service-attrs)
+        new-module (merge module {:content new-content})]
+    (crud/edit {:params         {:uuid          (u/id->uuid module-id)
+                                 :resource-name m/resource-type}
+                :request-method :put
+                :body           new-module
+                :nuvla/authn    auth/internal-identity})))
 
 
 (defmethod callback/execute action-name
