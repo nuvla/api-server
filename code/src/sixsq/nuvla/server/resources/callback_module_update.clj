a29251f19ae17020a5952206f7cd238e89ee7e6b
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2019-08-13 16:45:59 +0200

    better way to get and edit :content of module

--- code/src/sixsq/nuvla/server/resources/callback_module_update.clj
+++ code/src/sixsq/nuvla/server/resources/callback_module_update.clj
@@ -1,13 +1,11 @@
 (ns sixsq.nuvla.server.resources.callback-module-update
   (:require
-    [clojure.tools.logging :as log]
     [sixsq.nuvla.auth.utils :as auth]
     [sixsq.nuvla.server.resources.notification :refer [resource-type]]
     [sixsq.nuvla.server.resources.callback :as callback]
     [sixsq.nuvla.server.resources.common.crud :as crud]
     [sixsq.nuvla.server.resources.common.utils :as u]
-    [sixsq.nuvla.server.resources.module :as m]
-    [sixsq.nuvla.server.util.response :as r]))
+    [sixsq.nuvla.server.resources.module :as m]))
 
 
 (def ^:const action-name "module-update")
@@ -15,15 +13,11 @@
 
 (defn update-component!
   [module-id data]
-  (let [module (crud/retrieve-by-id module-id)
-        new-content (-> module
-                        :versions
-                        last
-                        :href
-                        crud/retrieve-by-id
-                        (merge data)
-                        u/strip-service-attrs)
-        new-module (merge module {:content new-content})]
+  (let [module (:body (crud/retrieve {:params         {:uuid          (u/id->uuid module-id)
+                                                       :resource-name m/resource-type}
+                                      :request-method :get
+                                      :nuvla/authn    auth/internal-identity}))
+        new-module (update module :content #(merge % data))]
     (crud/edit {:params         {:uuid          (u/id->uuid module-id)
                                  :resource-name m/resource-type}
                 :request-method :put
@@ -33,7 +27,4 @@
 
 (defmethod callback/execute action-name
   [{{id :href} :target-resource :as callback-resource} request]
-  (let [msg (str id " successfully updated")]
-    (update-component! id (:data callback-resource))
-    (log/info msg)
-    (r/map-response msg 200 id)))
+  (update-component! id (:data callback-resource)))
