d2bbcc508a903024883c080255342dee78e83ba9
khaled basbous <khaled@sixsq.com>
2019-09-18 13:17:48 +0200

    release 4.2.4

--- code/project.clj
+++ code/project.clj
@@ -1,7 +1,7 @@
 (def parent-version "6.6.0")
 (def nuvla-ring-version "2.0.2")
 
-(defproject sixsq.nuvla.server/api-jar "4.2.4-SNAPSHOT"
+(defproject sixsq.nuvla.server/api-jar "4.2.4"
 
   :description "core api server"
 
