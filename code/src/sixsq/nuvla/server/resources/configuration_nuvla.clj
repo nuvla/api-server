0392998baa12a8ee96e2dac0a0fe066eff13da7c
khaled basbous <khaled@sixsq.com>
2020-05-29 09:02:18 +0200

    load stripe api-key from env

--- code/src/sixsq/nuvla/server/resources/configuration_nuvla.clj
+++ code/src/sixsq/nuvla/server/resources/configuration_nuvla.clj
@@ -5,15 +5,16 @@ exist. If it isn't found when the server starts, then it will be created with
 default values.
 "
   (:require
+    [clojure.tools.logging :as log]
+    [environ.core :as env]
+    [sixsq.nuvla.server.resources.common.crud :as crud]
     [sixsq.nuvla.server.resources.common.std-crud :as std-crud]
     [sixsq.nuvla.server.resources.common.utils :as u]
     [sixsq.nuvla.server.resources.configuration :as p]
     [sixsq.nuvla.server.resources.configuration-template :as ct]
     [sixsq.nuvla.server.resources.configuration-template-nuvla :as tpl-nuvla]
-    [sixsq.nuvla.server.resources.spec.configuration-template-nuvla :as ct-nuvla]
-    [sixsq.nuvla.server.resources.common.crud :as crud]
     [sixsq.nuvla.server.resources.pricing.stripe :as stripe]
-    [clojure.tools.logging :as log]
+    [sixsq.nuvla.server.resources.spec.configuration-template-nuvla :as ct-nuvla]
     [sixsq.nuvla.server.util.response :as r]))
 
 
@@ -51,9 +52,10 @@ default values.
                          :template      {:href tpl-instance-url}}]
     (std-crud/add-if-absent config-instance-url p/resource-type create-template)
     (try
-      (when-let [stripe-api-key (-> config-instance-url
-                                    crud/retrieve-by-id-as-admin
-                                    :stripe-api-key)]
+      (when-let [stripe-api-key (or (-> config-instance-url
+                                        crud/retrieve-by-id-as-admin
+                                        :stripe-api-key)
+                                    (env/env :stripe-api-key))]
         (stripe/set-api-key! stripe-api-key)
         (alter-var-root #'*stripe-api-key* (constantly stripe-api-key)))
       (catch Exception e
