074ef3fa53e81b57d6011e2cc6c900a45db55f7d
khaled basbous <khaled@sixsq.com>
2020-09-02 14:05:00 +0200

    fix bug in groups refresh

--- code/src/sixsq/nuvla/server/resources/session.clj
+++ code/src/sixsq/nuvla/server/resources/session.clj
@@ -374,7 +374,7 @@ status, a 'set-cookie' header, and a 'location' header with the created
         expires        (ts/rfc822->iso8601 (:expires cookie))
         session        (assoc session :expiry expires
                                       :active-claim claim
-                                      :groups (:groups cookie)
+                                      :groups (:groups cookie-info)
                                       :roles updated-roles)]
     (-> request
         (assoc :body session)
