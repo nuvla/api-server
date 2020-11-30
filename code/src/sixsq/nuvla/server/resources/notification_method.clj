afd800cc2b9d8f9d14bef340119d8349cb8f742c
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2020-11-30 22:22:04 +0100

    notification method

---
+++ code/src/sixsq/nuvla/server/resources/notification_method.clj
@@ -0,0 +1,138 @@
+(ns sixsq.nuvla.server.resources.notification-method
+  "
+Collection for holding notification method configurations.
+"
+  (:require
+    [clojure.tools.logging :as log]
+    [sixsq.nuvla.auth.acl-resource :as a]
+    [sixsq.nuvla.db.impl :as db]
+    [sixsq.nuvla.db.filter.parser :as parser]
+    [sixsq.nuvla.server.resources.common.crud :as crud]
+    [sixsq.nuvla.server.resources.common.std-crud :as std-crud]
+    [sixsq.nuvla.server.resources.common.utils :as u]
+    [sixsq.nuvla.server.resources.resource-metadata :as md]
+    [sixsq.nuvla.server.resources.spec.notification-method :as notif-method-schema]
+    [sixsq.nuvla.server.util.kafka :as ka]
+    [sixsq.nuvla.server.util.kafka-crud :as ka-crud]
+    [sixsq.nuvla.server.util.metadata :as gen-md]
+    [sixsq.nuvla.server.util.response :as ru]))
+
+
+(def ^:const resource-type (u/ns->type *ns*))
+
+
+(def ^:const collection-type (u/ns->collection-type *ns*))
+
+
+(def collection-acl {:query ["group/nuvla-user"]
+                     :add   ["group/nuvla-user"]})
+
+
+;;
+;; initialization
+;;
+
+(def resource-metadata (gen-md/generate-metadata ::ns ::notif-method-schema/schema))
+
+
+(defn initialize
+  []
+  (std-crud/initialize resource-type ::notif-method-schema/schema)
+  (md/register resource-metadata))
+
+
+;;
+;; validation
+;;
+
+(def validate-fn (u/create-spec-validation-fn ::notif-method-schema/schema))
+
+
+(defmethod crud/validate resource-type
+  [resource]
+  (validate-fn resource))
+
+
+;;
+;; use default ACL method
+;;
+
+(defmethod crud/add-acl resource-type
+  [resource request]
+  (a/add-acl resource request))
+
+
+;;
+;; CRUD operations
+;;
+
+(def add-impl (std-crud/add-fn resource-type collection-acl resource-type))
+
+(defmethod crud/add resource-type
+  [request]
+  (let [resp (add-impl request)]
+    (ka-crud/publish-on-add resource-type resp)
+    resp))
+
+
+(def retrieve-impl (std-crud/retrieve-fn resource-type))
+
+
+(defmethod crud/retrieve resource-type
+  [request]
+  (retrieve-impl request))
+
+
+(def edit-impl (std-crud/edit-fn resource-type))
+
+
+(defmethod crud/edit resource-type
+  [request]
+  (let [resp (edit-impl request)]
+    (ka-crud/publish-on-edit resource-type resp)
+    resp))
+
+
+(defn throw-references-exist
+  [resource-id collections]
+  (doseq [collection collections]
+    (let [filter (format "method='%s'" resource-id)
+          options {:cimi-params {:filter (parser/parse-cimi-filter filter) :last 0}}
+          [meta _] (crud/query-as-admin collection options)]
+      (if (> (:count meta) 0)
+        (throw (ru/ex-conflict (format "References to %s exist in %s." resource-id collection)))))))
+
+(defn integrity-check
+  [resource-id collections]
+  (throw-references-exist resource-id collections)
+  resource-id)
+
+(def collections ["subscription"
+                  "subscription-config"])
+
+(defn delete-impl
+  [{{uuid :uuid} :params :as request}]
+  (try
+    (let [resource-id (str resource-type "/" uuid)
+          delete-response (-> resource-id
+                              (integrity-check collections)
+                              (db/retrieve request)
+                              (a/throw-cannot-delete request)
+                              (db/delete request))]
+      (ka-crud/publish-tombstone resource-type resource-id)
+      delete-response)
+    (catch Exception e
+      (or (ex-data e) (throw e)))))
+
+(defmethod crud/delete resource-type
+  [request]
+  (delete-impl request))
+
+
+(def query-impl (std-crud/query-fn resource-type collection-acl collection-type))
+
+
+(defmethod crud/query resource-type
+  [request]
+  (query-impl request))
+
---
+++ code/test/sixsq/nuvla/server/resources/notification_method_test.clj
@@ -0,0 +1,152 @@
+(ns sixsq.nuvla.server.resources.notification-method-test
+  (:require
+    [clojure.data.json :as json]
+    [clojure.test :refer [deftest is join-fixtures use-fixtures]]
+    [peridot.core :refer [content-type header request session]]
+    [sixsq.nuvla.server.app.params :as p]
+    [sixsq.nuvla.server.middleware.authn-info :refer [authn-info-header]]
+    [sixsq.nuvla.server.resources.notification-method :as t]
+    [sixsq.nuvla.server.resources.subscription :as subs]
+    [sixsq.nuvla.server.resources.lifecycle-test-utils :as ltu])
+  (:import
+    [java.util UUID]))
+
+
+(use-fixtures :once ltu/with-test-server-kafka-fixture)
+
+(def base-uri (str p/service-context t/resource-type))
+
+(def subscription-base-uri (str p/service-context subs/resource-type))
+
+(def valid-notif-conf {:method      "email"
+                       :destination "jane@example.com"
+                       :acl         {:owners ["user/jane"]}})
+
+(deftest lifecycle
+  (let [session-anon (-> (ltu/ring-app)
+                         session
+                         (content-type "application/json"))
+        session-admin (header session-anon authn-info-header "user/super group/nuvla-admin group/nuvla-user group/nuvla-anon")
+        session-user (header session-anon authn-info-header "user/jane group/nuvla-user group/nuvla-anon")]
+    (doseq [session [session-admin session-user]]
+      (-> session
+          (request base-uri)
+          (ltu/body->edn)
+          (ltu/is-status 200)
+          (ltu/is-count zero?)
+          (ltu/is-operation-present :add)
+          (ltu/is-operation-absent :delete)
+          (ltu/is-operation-absent :edit)))
+
+    ;; anon query fails
+    (-> session-anon
+        (request base-uri)
+        (ltu/body->edn)
+        (ltu/is-status 403))
+
+    ;; anon create must fail
+    (-> session-anon
+        (request base-uri
+                 :request-method :post
+                 :body (json/write-str valid-notif-conf))
+        (ltu/body->edn)
+        (ltu/is-status 403))
+
+    ;; check creation
+    (let [user-uri (-> session-user
+                       (request base-uri
+                                :request-method :post
+                                :body (json/write-str valid-notif-conf))
+                       (ltu/body->edn)
+                       (ltu/is-status 201)
+                       (ltu/location))
+
+          user-abs-uri (str p/service-context user-uri)]
+
+      (-> session-user
+          (request base-uri)
+          (ltu/body->edn)
+          (ltu/is-status 200)
+          (ltu/is-resource-uri t/collection-type)
+          (ltu/is-count 1))
+
+      ;; verify that an edit works
+      (let [method "slack"
+            dest "https://slack.com"
+            updated (assoc valid-notif-conf :method method :destination dest)]
+
+        (-> session-user
+            (request user-abs-uri
+                     :request-method :put
+                     :body (json/write-str updated))
+            (ltu/body->edn)
+            (ltu/is-status 200)
+            (ltu/body))
+
+        (let [updated-body (-> session-admin
+                               (request user-abs-uri)
+                               (ltu/body->edn)
+                               (ltu/is-status 200)
+                               (ltu/body))]
+
+          (is (= method (:method updated-body)))
+          (is (= dest (:destination updated-body)))))
+
+      ;; user can delete the data-set
+      (-> session-user
+          (request user-abs-uri :request-method :delete)
+          (ltu/body->edn)
+          (ltu/is-status 200)))))
+
+(deftest lifecycle-with-subscription
+  (let [session-anon (-> (ltu/ring-app)
+                         session
+                         (content-type "application/json"))
+        session-user (header session-anon authn-info-header "user/jane group/nuvla-user group/nuvla-anon")
+
+        ; create notification configuration method
+        notif-uri (-> session-user
+                      (request base-uri
+                               :request-method :post
+                               :body (json/write-str valid-notif-conf))
+                      (ltu/body->edn)
+                      (ltu/is-status 201)
+                      (ltu/location))
+
+        notif-abs-uri (str p/service-context notif-uri)
+
+        ; create subscription referencing the notification method
+        subscription {:type      "notification"
+                      :status    "enabled"
+                      :kind      "event"
+                      :category  "state"
+                      :resource  (str "infrastructure-service/" (str (UUID/randomUUID)))
+                      :method    notif-uri
+                      :acl       {:owners ["user/jane"]}}
+
+        subs-uri (-> session-user
+                     (request subscription-base-uri
+                              :request-method :post
+                              :body (json/write-str subscription))
+                     (ltu/body->edn)
+                     (ltu/is-status 201)
+                     (ltu/location))
+        subs-abs-uri (str p/service-context subs-uri)]
+
+    ; can't delete because of subscription referencing the notification method
+    (-> session-user
+        (request notif-abs-uri :request-method :delete)
+        (ltu/body->edn)
+        (ltu/is-status 409))
+
+    ; delete subscription
+    (-> session-user
+        (request subs-abs-uri :request-method :delete)
+        (ltu/body->edn)
+        (ltu/is-status 200))
+
+    ; now can delete notification method
+    (-> session-user
+        (request notif-abs-uri :request-method :delete)
+        (ltu/body->edn)
+        (ltu/is-status 200))))
