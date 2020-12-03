f4f37f3ba3207e0878e47822872ddfe26abaede3
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2020-12-03 08:25:52 +0100

    remove erroneous publication of events from event util

--- code/src/sixsq/nuvla/server/resources/event/utils.clj
+++ code/src/sixsq/nuvla/server/resources/event/utils.clj
@@ -1,11 +1,9 @@
 (ns sixsq.nuvla.server.resources.event.utils
   (:require
-    [clojure.tools.logging :as log]
     [sixsq.nuvla.auth.utils :as auth]
     [sixsq.nuvla.server.resources.common.crud :as crud]
     [sixsq.nuvla.server.resources.event :as event]
-    [sixsq.nuvla.server.util.time :as time]
-    [sixsq.nuvla.server.util.kafka :as ka]))
+    [sixsq.nuvla.server.util.time :as time]))
 
 
 (def topic event/resource-type)
@@ -25,12 +23,5 @@
                         :acl           acl}
         create-request {:params      {:resource-name event/resource-type}
                         :body        event-map
-                        :nuvla/authn auth/internal-identity}
-        ret             (crud/add create-request)]
-    (try
-        (log/info (format "Kafka. Publishing async event: %s %s %s" topic category event-map))
-        (ka/publish-async topic category event-map)
-      (catch Exception e
-        (log/error (format "Kafka. Failed publishing to %s topic with key %s: %s" topic category e)))
-        (finally
-          ret))))
+                        :nuvla/authn auth/internal-identity}]
+    (crud/add create-request)))
