fc68ccc02de38fb3ae2d5711f822798a93df0e43
khaled basbous <khaled@sixsq.com>
2021-06-09 14:03:48 +0200

    release 5.14.2

--- code/project.clj
+++ code/project.clj
@@ -1,7 +1,7 @@
 (def parent-version "6.7.5")
 (def nuvla-ring-version "2.0.2")
 
-(defproject sixsq.nuvla.server/api-jar "5.14.2-SNAPSHOT"
+(defproject sixsq.nuvla.server/api-jar "5.14.2"
 
   :description "core api server"
 
