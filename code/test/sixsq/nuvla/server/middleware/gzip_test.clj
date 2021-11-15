bf978f8114110df1eeff91c8a67595ff77caa3ca
khaled basbous <khaled@sixsq.com>
2021-11-15 17:19:54 +0100

    unit tests

---
+++ code/test/sixsq/nuvla/server/middleware/gzip_test.clj
@@ -0,0 +1,22 @@
+(ns sixsq.nuvla.server.middleware.gzip-test
+  (:require
+    [clojure.string :as str]
+    [clojure.test :refer [deftest is]]
+    [peridot.request :as pr]
+    [sixsq.nuvla.server.middleware.gzip :as t]))
+
+
+(defn request
+  [headers]
+  (pr/build "/"  nil headers nil nil))
+
+(deftest detect-request-gzipped
+  (is (nil? (t/content-encoding (request {}))))
+  (is (= (t/content-encoding (request {:content-encoding "gzip"})) "gzip"))
+  (is (= (t/content-encoding (request {:Content-Encoding "gzip"})) "gzip")))
+
+(deftest test-gzip?
+  (is (false?(t/gzip? (request {}))))
+  (is (t/gzip? (request {:content-encoding "gzip"})))
+  (is (t/gzip? (request {:Content-Encoding "gzip"}))))
+
