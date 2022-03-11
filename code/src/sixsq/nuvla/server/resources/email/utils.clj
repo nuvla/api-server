85d60667aed4d66846cf67e1afdef8110277f26a
Fabian Merkle <merklefabian@googlemail.com>
2022-03-11 11:34:05 +0700

    Add end trial email text, restructure trial email data

--- code/src/sixsq/nuvla/server/resources/email/utils.clj
+++ code/src/sixsq/nuvla/server/resources/email/utils.clj
@@ -252,39 +252,6 @@
     (send-email nuvla-config msg)))
 
 
-(defn email-data-trial-ending [trial-days-left resources url]
-  (let [pre-texts  (conj [(format "Your Nuvla.io trial is will end in %d days. Now is a good time to upgrade and get all the awesome benefits." 
-                                  trial-days-left)
-                          "Your account will be deactivated when the trial ends."]
-                         (when (seq resources) "The following resources will be frozen: "))
-        post-texts ["You’re also going to lose access to any data you have in Nuvla.io. You no longer will be able to receive data from your Nuvla Edge devices on Nuvla.io."
-                    "Do not worry! We keep it safe with us for when you decide to upgrade."
-                    "By upgrading now, you will ensure your data flows stays active. Just add a payment method in your Nulva.io account."
-                    (format "Luckily you still have %d days to upgrade." trial-days-left)]]
-    {:subject "Nuvla trial ending"
-     :body    [:alternative
-               {:type    "text/plain"
-                :content (str/join "\n" 
-                                   (concat pre-texts 
-                                           (map (partial str "- ")
-                                                resources) 
-                                           post-texts))}
-               {:type    "text/html; charset=utf-8"
-                :content (render-email
-                           {:title          "Your Nuvla.io trial is ending"
-                            :button-text    "Choose a Novla.io plan today:"
-                            :button-url     url
-                            :text-1        (str "<p>" 
-                                                (str/join "</p><p>" 
-                                                          (concat pre-texts 
-                                                                  (when (seq resources) 
-                                                                    (str "<ul><li>" 
-                                                                         (str/join "</li><li>" resources) 
-                                                                         "</li></ul>")) 
-                                                                  post-texts))
-                                                "</p>")})}]}))
-
-
 (defn send-text-email [address email-data]
   (let [{:keys [smtp-username] :as nuvla-config} (crud/retrieve-by-id-as-admin
                                                    config-nuvla/config-instance-url)]
