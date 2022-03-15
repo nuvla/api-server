09d3185fa17f7ec31534d432097ecbda225900c4
Fabian Merkle <merklefabian@googlemail.com>
2022-03-15 15:35:39 +0700

    Add email template tests, add resources to trial email

--- code/src/sixsq/nuvla/server/resources/email/text.clj
+++ code/src/sixsq/nuvla/server/resources/email/text.clj
@@ -1,26 +1,28 @@
-(ns sixsq.nuvla.server.resources.email.text
-  (:require
-    [clojure.string :as str]
-    [sixsq.nuvla.server.resources.email.utils :refer [render-email]]))
+(ns sixsq.nuvla.server.resources.email.text)
 
-(defn trial-ending [{:keys [trial-days-left]}]
-  {:template      :trial
-   :subject       "Nuvla trial ending"
-   :title         "Your Nuvla trial is ending"
-   :button-text   "Add your payment methods"
-   :button-url    "https://nuvla.io"
-   :text-1        (format "Your Nuvla.io trial will end in %d days. Make sure you have a payement method set, to avoid that your resources are frozen when the trial ends." 
-                          trial-days-left)
-   ; :resources-text-1 "The following resources will be frozen: "
-   :text-2        "You’re also going to lose access to any data you have in Nuvla. You no longer will be able to receive data from your Nuvla Edge devices on Nuvla.io. Do not worry! We keep it safe with us for when you set your payment method."
-   :text-3        (format "By entering an payment method, you will ensure your data flows stays active. Just add a payment method in your Nulva.io account. Luckily you still have %d days." 
-                          trial-days-left)})
+(defn trial-ending [{:keys [trial-days-left resources]}]
+  {:template         :trial
+   :subject          "Nuvla trial ending"
+   :title            "Your Nuvla trial is ending"
+   :button-intro     "Add your payment method to use your resources:"
+   :button-text      "Add your payment methods"
+   :button-url       "https://nuvla.io"
+   :text-1           (format "Your Nuvla.io trial will end in %d days. Make sure you have a payement method set, to avoid that your resources are frozen when the trial ends." 
+                             trial-days-left)
+   :resources-title  "The following resources will be frozen: "
+   :resources        resources
+   :text-2           "You’re also going to lose access to any data you have in Nuvla. You no longer will be able to receive data from your Nuvla Edge devices on Nuvla.io. Do not worry! We keep it safe with us for when you set your payment method."
+   :text-3           (format "By entering an payment method, you will ensure your data flows stays active. Just add a payment method in your Nulva.io account. Luckily you still have %d days." 
+                             trial-days-left)})
 
-(defn trial-ended [{:keys [resources-frozen?]}]
-  {:template      :trial
-   :subject        "Nuvla trial ended"
-   :title          "Your Nuvla trial ended"
-   :button-text    "Add your payment methods"
-   :button-url     "https://nuvla.io/profile"
-   :text-1         (str "Your Nuvla trail expired today. " (when resources-frozen? "If you want to unfreeze your resources login to Nuvla.io and unfreeze them from your profile page.")) 
-   :text-3         "Would you mind sharing briefly with us what kept your from adopting Nuvla.io? Feel free to reach us in the live chat in Nuvla.io. Would really love to hear your feedback. Thank you."})
+(defn trial-ended [{:keys [resources]}]
+  {:template         :trial
+   :subject          "Nuvla trial ended"
+   :title            "Your Nuvla trial ended"
+   :button-intro     "Add your payment method to use your resources:"
+   :button-text      "Add your payment methods"
+   :button-url       "https://nuvla.io/profile"
+   :resources-title  "The following resources are now frozen: "
+   :resources        resources
+   :text-1           (str "Your Nuvla trail expired today. " (when (seq resources) "If you want to unfreeze your resources login to Nuvla.io and unfreeze them from your profile page.")) 
+   :text-3           "Would you mind sharing briefly with us what kept your from adopting Nuvla.io? Feel free to reach us in the live chat in Nuvla.io. Would really love to hear your feedback. Thank you."})
--- code/test/sixsq/nuvla/server/resources/email/text_test.clj
+++ code/test/sixsq/nuvla/server/resources/email/text_test.clj
@@ -1,7 +1,8 @@
 (ns sixsq.nuvla.server.resources.email.text-test
   (:require
-    [clojure.java.io :refer [writer reader resource]]
-    [clojure.test :refer :all]
+    [clojure.java.io :refer [writer reader]]
+    [clojure.string :as str]
+    [clojure.test :refer [deftest testing is]]
     [sixsq.nuvla.server.resources.email.text :as t]
     [sixsq.nuvla.server.resources.email.utils :as u]))
 
@@ -10,12 +11,35 @@
     (.write wrtr input)
     (.close wrtr)))
 
+(defn email-render [email-data]
+  (-> email-data
+      u/email-render
+      :body 
+      last
+      :content))
+
+(def resources [{:id    "873ab926"
+                 :url   "/var/folders/qc/4xjzgdw17z32fkb3x36nrv6m0000gp/T/clojure-8514830771576825996.edn"
+                 :title "Nginx simple app"
+                 :kind  "Deployment"}])
+
 (comment
-  (-> #_(t/trial-ending {:trial-days-left 5})
-      (t/trial-ended true )
+  ; use this tool to create the end result for the test
+  (-> #_(t/trial-ending {:trial-days-left 6
+                       :resources resources})
+      (t/trial-ended {:resources resources})
 
       u/email-render
       :body 
       last
-     :content
+      :content
       (write "test-resources/email-trial-ended.html")))
+
+(deftest trial-emails
+  (testing "trail ending email content should match fixed end result"
+    (is (= (email-render (t/trial-ending {:trial-days-left 6
+                                          :resources resources})) 
+           (slurp  "test-resources/email-trial-ending.html"))))
+  (testing "trail end email content should match fixed end result"
+    (is (= (email-render (t/trial-ended {:resources resources})) 
+           (slurp  "test-resources/email-trial-ended.html")))))
