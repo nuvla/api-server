c2613b8b306843a8019a7e73906792b0c950cb8d
khaled basbous <khaled@sixsq.com>
2022-03-15 10:56:15 +0100

    multi resource email test case

--- code/test/sixsq/nuvla/server/resources/email/text_test.clj
+++ code/test/sixsq/nuvla/server/resources/email/text_test.clj
@@ -1,8 +1,7 @@
 (ns sixsq.nuvla.server.resources.email.text-test
   (:require
-    [clojure.java.io :refer [writer reader]]
-    [clojure.string :as str]
-    [clojure.test :refer [deftest testing is]]
+    [clojure.java.io :refer [reader writer]]
+    [clojure.test :refer [deftest is testing]]
     [sixsq.nuvla.server.resources.email.text :as t]
     [sixsq.nuvla.server.resources.email.utils :as u]))
 
@@ -14,32 +13,43 @@
 (defn email-render [email-data]
   (-> email-data
       u/email-render
-      :body 
+      :body
       last
       :content))
 
-(def resources [{:id    "873ab926"
-                 :url   "/var/folders/qc/4xjzgdw17z32fkb3x36nrv6m0000gp/T/clojure-8514830771576825996.edn"
+(def resource-1 {:id    "61467019"
+                 :url   "https://nuvla.io/ui/deployment/61467019-2931-463d-b32c-c60a73c205e5"
                  :title "Nginx simple app"
-                 :kind  "Deployment"}])
+                 :kind  "Deployment"})
+
+(def resource-2 {:id    "71467019"
+                 :url   "https://nuvla.io/ui/edge/71467019-2931-463d-b32c-c60a73c205e5"
+                 :title "My first NuvlaBox"
+                 :kind  "NuvlaBox"})
+
+(def resources [resource-1])
 
 (comment
   ; use this tool to create the end result for the test
   (-> #_(t/trial-ending {:trial-days-left 6
-                       :resources resources})
-      (t/trial-ended {:resources resources})
+                         :resources       resources})
+    (t/trial-ended {:resources (conj resources resource-2)})
 
-      u/email-render
-      :body 
-      last
-      :content
-      (write "test-resources/email-trial-ended.html")))
+    u/email-render
+    :body
+    last
+    :content
+    (write "test-resources/email/trial-ended-multi.html")))
 
 (deftest trial-emails
   (testing "trail ending email content should match fixed end result"
     (is (= (email-render (t/trial-ending {:trial-days-left 6
-                                          :resources resources})) 
-           (slurp  "test-resources/email-trial-ending.html"))))
+                                          :resources       resources}))
+           (slurp "test-resources/email/trial-ending.html"))))
   (testing "trail end email content should match fixed end result"
-    (is (= (email-render (t/trial-ended {:resources resources})) 
-           (slurp  "test-resources/email-trial-ended.html")))))
+    (is (= (email-render (t/trial-ended {:resources resources}))
+           (slurp "test-resources/email/trial-ended.html"))))
+
+  (testing "trail end email with more than one resource email"
+    (is (= (email-render (t/trial-ended {:resources (conj resources resource-2)}))
+           (slurp "test-resources/email/trial-ended-multi.html")))))
