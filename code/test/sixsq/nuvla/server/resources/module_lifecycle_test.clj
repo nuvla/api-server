ecd463d47955e8dacf4e8f3f09d3fb03c63af6fe
khaled basbous <khaled@sixsq.com>
2023-04-13 09:15:01 +0200

    fix tests

--- code/test/sixsq/nuvla/server/resources/module_lifecycle_test.clj
+++ code/test/sixsq/nuvla/server/resources/module_lifecycle_test.clj
@@ -114,13 +114,10 @@
                           (request abs-uri)
                           (ltu/body->edn)
                           (ltu/is-status 200)
+                          (ltu/is-key-value :compatibility "docker-compose")
                           (as-> m (if (utils/is-application? subtype)
-                                    (do
-                                      (ltu/is-key-value m :compatibility "docker-compose")
-                                      (ltu/is-operation-present m :validate-docker-compose))
-                                    (do
-                                      (ltu/is-key-value m :compatibility nil)
-                                      (ltu/is-operation-absent m :validate-docker-compose))))
+                                    (ltu/is-operation-present m :validate-docker-compose)
+                                    (ltu/is-operation-absent m :validate-docker-compose)))
                           (ltu/body)
                           :content)]
           (is (= valid-content (select-keys content (keys valid-content)))))
