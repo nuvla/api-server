f40a803cec9f7cac5f9eb202ad709f430e06d10e
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2024-01-18 20:47:35 +0100

    next development version

--- code/project.clj
+++ code/project.clj
@@ -2,7 +2,7 @@
 (def nuvla-ring-version "2.0.8")
 (def kinsky-version "0.3.1")
 
-(defproject sixsq.nuvla.server/api-jar "6.4.3"
+(defproject sixsq.nuvla.server/api-jar "6.4.4-SNAPSHOT"
 
   :description "core api server"
 
