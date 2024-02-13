218fc7a338f09c91f7a561ae65c6897d140b48e5
Khaled Basbous <khaled@sixsq.com>
2024-02-13 09:12:32 +0100

    Deployment-parameter can be created by anyone with any parent (deployment) (#876)

--- code/test/sixsq/nuvla/server/resources/deployment_parameter_lifecycle_test.clj
+++ code/test/sixsq/nuvla/server/resources/deployment_parameter_lifecycle_test.clj
@@ -1,10 +1,11 @@
 (ns sixsq.nuvla.server.resources.deployment-parameter-lifecycle-test
   (:require
     [clojure.data.json :as json]
-    [clojure.test :refer [deftest is use-fixtures]]
+    [clojure.test :refer [deftest testing is use-fixtures]]
     [peridot.core :refer [content-type header request session]]
     [sixsq.nuvla.server.app.params :as p]
     [sixsq.nuvla.server.middleware.authn-info :refer [authn-info-header]]
+    [sixsq.nuvla.server.resources.common.resource-creation :as resource-creation]
     [sixsq.nuvla.server.resources.common.utils :as u]
     [sixsq.nuvla.server.resources.deployment-parameter :as t]
     [sixsq.nuvla.server.resources.lifecycle-test-utils :as ltu]
@@ -18,33 +19,9 @@
 
 
 (def parent-id "deployment/324c6138-aaaa-bbbb-cccc-af3ad15815db")
-
-
 (def node-id "machine")
-
-
 (def parameter-name "param1")
-
-
 (def parameter-name-user "param.user")
-
-
-(def valid-entry
-  {:name    parameter-name
-   :parent  parent-id
-   :node-id node-id
-   :acl     {:owners   ["group/nuvla-admin"]
-             :edit-acl ["user/jane"]}})
-
-
-(def valid-entry-user
-  {:name    parameter-name-user
-   :parent  parent-id
-   :node-id node-id
-   :acl     {:owners   ["user/jane"]}})
-
-
-(def expected-dp-id (str "deployment-parameter/" (t/parameter->uuid parent-id node-id parameter-name)))
 
 
 (deftest check-metadata
@@ -52,48 +29,68 @@
 
 
 (deftest lifecycle
-  (let [session       (-> (ltu/ring-app)
-                          session
-                          (content-type "application/json"))
-        session-admin (header session authn-info-header "group/nuvla-admin group/nuvla-admin group/nuvla-user group/nuvla-anon")
-        session-jane  (header session authn-info-header "user/jane user/jane group/nuvla-user group/nuvla-anon")
-        session-anon  (header session authn-info-header "user/unknown user/unknown group/nuvla-anon")]
-
-    ;; admin user collection query should succeed but be empty (no records created yet)
-    (-> session-admin
-        (request base-uri)
-        (ltu/body->edn)
-        (ltu/is-status 200)
-        (ltu/is-count zero?)
-        (ltu/is-operation-present :add)
-        (ltu/is-operation-absent :delete)
-        (ltu/is-operation-absent :edit))
-
-    ;; normal user collection query should succeed but be empty (no records created yet)
-    (-> session-jane
-        (request base-uri)
-        (ltu/body->edn)
-        (ltu/is-status 200)
-        (ltu/is-count zero?)
-        (ltu/is-operation-present :add)
-        (ltu/is-operation-absent :delete)
-        (ltu/is-operation-absent :edit))
+  (let [session          (-> (ltu/ring-app)
+                             session
+                             (content-type "application/json"))
+        session-admin    (header session authn-info-header "group/nuvla-admin group/nuvla-admin group/nuvla-user group/nuvla-anon")
+        session-jane     (header session authn-info-header "user/jane user/jane group/nuvla-user group/nuvla-anon")
+        session-tarzan   (header session authn-info-header "user/tarzan user/tarzan group/nuvla-user group/nuvla-anon")
+        session-anon     (header session authn-info-header "user/unknown user/unknown group/nuvla-anon")
+        parent-id        (->> (resource-creation/create-module session-jane)
+                              (resource-creation/create-deployment session-jane))
+        expected-dp-id   (str "deployment-parameter/" (t/parameter->uuid parent-id node-id parameter-name))
+        valid-entry      {:name    parameter-name
+                          :parent  parent-id
+                          :node-id node-id
+                          :acl     {:owners   ["group/nuvla-admin"]
+                                    :edit-acl ["user/jane"]}}
+        valid-entry-user {:name    parameter-name-user
+                          :parent  parent-id
+                          :node-id node-id
+                          :acl     {:owners ["user/jane"]}}]
 
 
-    ;; anonymous credential collection query should not succeed
-    (-> session-anon
-        (request base-uri)
-        (ltu/body->edn)
-        (ltu/is-status 403))
+    (testing "admin user collection query should succeed but be empty (no records created yet)"
+      (-> session-admin
+          (request base-uri)
+          (ltu/body->edn)
+          (ltu/is-status 200)
+          (ltu/is-count zero?)
+          (ltu/is-operation-present :add)
+          (ltu/is-operation-absent :delete)
+          (ltu/is-operation-absent :edit)))
 
+    (testing "normal user collection query should succeed but be empty (no records created yet)"
+      (-> session-jane
+          (request base-uri)
+          (ltu/body->edn)
+          (ltu/is-status 200)
+          (ltu/is-count zero?)
+          (ltu/is-operation-present :add)
+          (ltu/is-operation-absent :delete)
+          (ltu/is-operation-absent :edit)))
 
-    ;; create a deployment parameter as a admin user
-    (let [resp-test     (-> session-admin
-                            (request base-uri
-                                     :request-method :post
-                                     :body (json/write-str valid-entry))
-                            (ltu/body->edn)
-                            (ltu/is-status 201))
+    (testing "anonymous credential collection query should not succeed"
+      (-> session-anon
+         (request base-uri)
+         (ltu/body->edn)
+         (ltu/is-status 403)))
+
+    (testing "A user without access to the parent deployment should not be allowed to create deployment parameter"
+      (-> session-tarzan
+          (request base-uri
+                   :request-method :post
+                   :body (json/write-str valid-entry))
+          (ltu/body->edn)
+          (ltu/is-status 403)))
+
+    (let [resp-test     (testing "create a deployment parameter as a admin user"
+                          (-> session-admin
+                              (request base-uri
+                                       :request-method :post
+                                       :body (json/write-str valid-entry))
+                              (ltu/body->edn)
+                              (ltu/is-status 201)))
 
           id-test       (ltu/body-resource-id resp-test)
 
@@ -101,26 +98,25 @@
 
           test-uri      (str p/service-context id-test)]
 
-      ; user can also create
-      (-> session-jane
-          (request base-uri
-                   :request-method :post
-                   :body (json/write-str valid-entry-user))
-          (ltu/body->edn)
-          (ltu/is-status 201))
+      (testing "user can also create"
+        (-> session-jane
+            (request base-uri
+                     :request-method :post
+                     :body (json/write-str valid-entry-user))
+            (ltu/body->edn)
+            (ltu/is-status 201)))
 
       (is (= location-test test-uri))
 
-      ;; admin should be able to see everyone's records. Deployment parameter id is predictable
-      (-> session-admin
-          (request test-uri)
-          (ltu/body->edn)
-          (ltu/is-status 200)
-          (ltu/is-id expected-dp-id)
-          (ltu/is-operation-present :delete)
-          (ltu/is-operation-present :edit))
+      (testing "admin should be able to see everyone's records. Deployment parameter id is predictable"
+        (-> session-admin
+            (request test-uri)
+            (ltu/body->edn)
+            (ltu/is-status 200)
+            (ltu/is-id expected-dp-id)
+            (ltu/is-operation-present :delete)
+            (ltu/is-operation-present :edit)))
 
-      ;; user can edit, but parent, name, and node-id cannot be changed
       (let [bad-id   "deployment/324c6138-0484-34b5-bf35-af3ad15815db"
             original (-> session-jane
                          (request test-uri)
@@ -128,15 +124,16 @@
                          (ltu/is-status 200)
                          (ltu/body))]
 
-        (-> session-jane
-            (request test-uri
-                     :request-method :put
-                     :body (json/write-str {:parent  bad-id
-                                            :name    "bad-name"
-                                            :node-id "bad-node"
-                                            :value   "OK!"}))
-            (ltu/body->edn)
-            (ltu/is-status 200))
+        (testing "user can edit, but parent, name, and node-id cannot be changed"
+          (-> session-jane
+              (request test-uri
+                       :request-method :put
+                       :body (json/write-str {:parent  bad-id
+                                              :name    "bad-name"
+                                              :node-id "bad-node"
+                                              :value   "OK!"}))
+              (ltu/body->edn)
+              (ltu/is-status 200)))
 
         (let [{:keys [id name node-id value]} (-> session-jane
                                                   (request test-uri)
@@ -162,25 +159,23 @@
           (ltu/body->edn)
           (ltu/is-status 403))
 
-      ;; search
-      (-> session-admin
-          (request base-uri)
-          (ltu/body->edn)
-          (ltu/is-status 200))
+      (testing "search"
+        (-> session-admin
+            (request base-uri)
+            (ltu/body->edn)
+            (ltu/is-status 200)))
 
-      ;; delete
-      (-> session-jane
-          (request test-uri
-                   :request-method :delete)
-          (ltu/body->edn)
-          (ltu/is-status 200))
-
-      ;; resource should be deleted
-      (-> session-admin
-          (request test-uri
-                   :request-method :delete)
-          (ltu/body->edn)
-          (ltu/is-status 404)))))
+      (testing "resource is deletable"
+        (-> session-jane
+            (request test-uri
+                     :request-method :delete)
+            (ltu/body->edn)
+            (ltu/is-status 200))
+        (-> session-admin
+            (request test-uri
+                     :request-method :delete)
+            (ltu/body->edn)
+            (ltu/is-status 404))))))
 
 
 (deftest bad-methods
