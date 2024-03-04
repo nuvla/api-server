275132769be380e7eb850bb3f8cada5a90f0f8ee
Alessandro Bellucci <alessandro.bellucci@sixsq.com>
2024-03-04 09:15:39 +0100

    Fix tests

--- code/test/sixsq/nuvla/server/resources/module_applications_sets_lifecycle_test.clj
+++ code/test/sixsq/nuvla/server/resources/module_applications_sets_lifecycle_test.clj
@@ -10,7 +10,7 @@
     [sixsq.nuvla.server.resources.module-applications-sets :as t]))
 
 
-(use-fixtures :once ltu/with-test-server-fixture)
+(use-fixtures :each ltu/with-test-server-fixture)
 
 
 (def base-uri (str p/service-context t/resource-type))
